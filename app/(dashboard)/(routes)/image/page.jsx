39d10f376b5275ae481c21299ff12936db955196
Neranjan Pushpakumara <neranjan.education@gmail.com>
2024-06-18 17:18:34 +0530

    image generation completely done.amazing job neranjan

---
+++ app/(dashboard)/(routes)/image/page.jsx
@@ -0,0 +1,196 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+import Heading from "@/components/Heading";
+import { Download, ImageIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { useRouter } from "next/navigation";
+
+import { amountOptions, formSchema, resolutionOptions } from "./constants";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Empty from "@/components/Empty";
+import Loader from "@/components/Loader";
+import { cn } from "@/lib/utils";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Card, CardFooter } from "@/components/ui/card";
+import Image from "next/image";
+
+export default function ImagePage() {
+  const router = useRouter();
+  const [images, setImages] = useState([]);
+
+  //MAX TOKENS =256
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+      amount: "1",
+      resolution: "256x256",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values) => {
+    try {
+      setImages([]);
+      const response = await axios.post("/api/image", values);
+
+      // const urls = response.data.map((image) => image.generated_image); //no need because we are getting only one image
+      const urls = [response.data.generated_image];
+
+
+      setImages(urls);
+
+      form.reset();
+    } catch (error) {
+      // TODO HANDLE PRO
+      console.log("Error in conversation page: ", error);
+    } finally {
+      router.refresh();
+    }
+  };
+  return (
+    <div>
+      <Heading
+        title="Image Generation"
+        description="This is Advanced AI Modal for Image Generation."
+        icon={ImageIcon}
+        iconColor="text-pink-700"
+        bgColor="bg-pink-700/10"
+      />
+      <div className="px-4 lg:px-8">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+          >
+            <FormField
+              control={form.control}
+              name="prompt"
+              render={({ field }) => (
+                <FormItem className="col-span-12 lg:col-span-6">
+                  <FormControl className="m-0 p-0">
+                    <Input
+                      className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                      disabled={isLoading}
+                      placeholder="A dog in a park."
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem className="col-span-12 lg:col-span-2">
+                  <Select
+                    disabled={isLoading}
+                    onValueChange={(value) => { field.onChange(value)}}
+                    value={field.value}
+                    defaultValue={field.value}
+                    
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue defaultValue={field.value} />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {amountOptions.map((option) => (
+                        <SelectItem key={option.value} value={option.value}>
+                          {option.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="resolution"
+              render={({ field }) => (
+                <FormItem className="col-span-12 lg:col-span-2">
+                  <Select
+                    disabled={isLoading}
+                    onValueChange={(value) => {field.onChange(value) }}
+                    value={field.value}
+                    defaultValue={field.value}
+                    
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue defaultValue={field.value} />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {resolutionOptions.map((option) => (
+                        <SelectItem key={option.value} value={option.value}>
+                          {option.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            />
+            <Button
+              type="submit"
+              className="col-span-12 lg:col-span-2 w-full"
+              disabled={isLoading}
+            >
+              Generate
+            </Button>
+          </form>
+        </Form>
+      </div>
+      <div className="space-y-4 mt-4">
+        {isLoading && (
+          <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+            <Loader />
+          </div>
+        )}
+        {images.length === 0 && !isLoading && (
+          <Empty label="No Images Generated." />
+        )}
+        <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8 mx-4 md:mx-8">
+          {images.map((src)=>(
+            <Card
+              key={src}
+              className="rounded-lg overflow-hidden"
+            >
+              <div className="relative aspect-square">
+                <Image
+                  src={src}
+                  fill
+                  alt="Image"
+                />
+              </div>
+              <CardFooter className="p-2">
+                <Button variant="secondary" className="w-full" onClick={()=>window.open(src)}>
+                  <Download className="h-4 w-4 mt-2"/>
+                  Download
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
---
+++ app/api/image/route.jsx
@@ -0,0 +1,110 @@
+
+// import { NextResponse } from "next/server";
+// import OpenAI from 'openai';
+// import { getAuth } from '@clerk/nextjs/server';
+
+
+// // Ensure your OpenAI API key is set in your environment variables
+// const configuration = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY
+// });
+
+// const openai = new OpenAIApi(configuration);
+// export default async function POST(req){
+//     try {
+//         const { userId } = getAuth(req);
+//         const body = await req.json();
+//         const {messages} = body;
+
+
+//         if(!userId){
+//             return new NextResponse("Unauthorized",{status:401})
+//         }
+
+//         if(!configuration.apiKey){
+//             return new NextResponse("OpenAI API Key not found",{status:500})
+//         }
+
+//         if(!messages){
+//             return new NextResponse("Messages not found",{status:400})
+//         }
+
+//         const response = await openai.createChatCompletion({
+//             model: "gpt-3.5-turbo",
+//             messages: messages
+//         })
+
+//         return NextResponse.json(response.data.choices[0].message)
+        
+//     } catch (error) {
+//         console.log("Error in conversation route: ", error)
+//         return new NextResponse("error",{status:500})
+//     }
+// }
+
+
+
+//---------------------------------------------------------------------------------------GPT-4-----------------------------------------
+
+import { NextResponse } from "next/server";
+import axios from 'axios';
+import { getAuth } from '@clerk/nextjs/server';
+
+// Ensure your RapidAPI key is set in your environment variables
+const RAPIDAPI_KEY = process.env.RAPIDAPI_KEY;
+
+if (!RAPIDAPI_KEY) {
+    throw new Error("RapidAPI Key not found in environment variables");
+}
+
+const RAPIDAPI_URL = 'https://chatgpt-42.p.rapidapi.com/texttoimage';
+const RAPIDAPI_HOST = 'chatgpt-42.p.rapidapi.com';
+
+export async function POST(req) {
+    try {
+        const { userId } = getAuth(req);
+        const body = await req.json();
+        const { amount,prompt,resolution } = body;
+
+        console.log("request body in route: ", body)
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        if (!amount) {
+            return new NextResponse("amount not found", { status: 400 });
+        }
+        if (!prompt) {
+            return new NextResponse("prompt not found", { status: 400 });
+        }
+        if (!resolution) {
+            return new NextResponse("resolution not found", { status: 400 });
+        }
+
+        let dimensions = resolution.split('x');
+        console.log("dimensions: ", dimensions);
+        
+        const options = {
+            method: 'POST',
+            url: RAPIDAPI_URL,
+            headers: {
+                'x-rapidapi-key': RAPIDAPI_KEY,
+                'x-rapidapi-host': RAPIDAPI_HOST,
+                'Content-Type': 'application/json'
+            },
+            data: {
+                text: prompt,
+                width: dimensions[0],
+                height: dimensions[1],
+            }
+        };
+
+        const response = await axios.request(options);
+        return NextResponse.json(response.data);
+
+    } catch (error) {
+        console.log("Error in conversation route: ", error);
+        return new NextResponse("error", { status: 500 });
+    }
+}
